86e5032251814fe5c8ff2635586f4eb68523ab11
Tomislav Pesut <tomca32@gmail.com>
2017-03-10 18:07:14 -0500

    Accepting the host option and handling invalid host error

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 'use strict';
 
-var argv = require('optimist').argv;
-var server = require('./api');
+const argv = require('optimist').argv;
+const server = require('./api');
 
-var port = Number(argv.port) || process.env.PORT || 3000;
+const port = Number(argv.port) || process.env.PORT || 3000;
+const host = argv.host || 'localhost';
+const invalidHostError = `This probably happened because of the invalid --host option.\nRemove the "--host ${argv.host}" option to run on localhost.`;
 
-server.listen(port, function () {
+server.listen(port, host, function () {
   console.log('Server running on port %d', port);
+}).on('error', function(err) {
+  if (err.errno === 'EADDRNOTAVAIL') {
+    console.error(`Error: ${err.message}\n${invalidHostError}`);
+  }
 });
