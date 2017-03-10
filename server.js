14dc424c543b699d8649a4a8246bc2d4a5d1ce23
Tomislav Pesut <tomca32@gmail.com>
2017-03-10 17:00:23 -0500

    Added port option parsing

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var argv = require('optimist').argv;
 var server = require('./api');
-var port = process.env.PORT || 3000;
+
+var port = Number(argv.port) || process.env.PORT || 3000;
 
 server.listen(port, function () {
   console.log('Server running on port %d', port);
